9ca8a6731dae39fc37f6ed64b2b241649edee4f2
Calil-Silva <ka.renner@hotmail.com>
2021-10-25 11:28:13 -0300

    bugs correction v2

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -10,20 +10,23 @@ async function login(req, res) {
 
     try {
         const user = await connection.query('SELECT id, name, password FROM users WHERE email = $1;', [email]);
+
         if(user.rows.length === 0) return res.status(404).send({message: "Usuário não encontrado"});
+
         if(bcrypt.compareSync(password, user.rows[0].password)) {
             let token = uuid();
+
             await connection.query('INSERT INTO logged_users (user_id, token) VALUES ($1, $2);', [user.rows[0].id, token]);
+
             return res.status(202).send({
                 name: user.rows[0].name,
                 token
             });
-
         } else {
             return res.status(403).send({message: "E-mail/senha incorretos"});
         }
     } catch (error) {
-        res.sendStatus(500);
+        res.status(500).send({message: "Ocorreu um erro inesperado"});
     }
 }
 
