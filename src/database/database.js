9396c449cce8d7903c9e3be1e3b0ab75fef7f68b
Calil-Silva <ka.renner@hotmail.com>
2021-10-31 16:41:23 -0300

    Updated port with parseInt and created env exemple

--- src/database/database.js
+++ src/database/database.js
@@ -2,11 +2,9 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-console.log(process.env.DB_DATABASE);
-
 const connection = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
