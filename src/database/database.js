10468b01290f391e825a505d46ac48c1b16c3a12
Calil-Silva <ka.renner@hotmail.com>
2021-11-01 06:48:05 -0300

    Prepare for deploy

--- src/database/database.js
+++ src/database/database.js
@@ -3,11 +3,10 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connection = new Pool({
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default connection;
