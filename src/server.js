91c905ce65322f4815812940e21f0fe4e7e4e0f7
Calil-Silva <ka.renner@hotmail.com>
2021-11-26 23:17:29 -0300

    Changed environment variables

--- src/server.js
+++ src/server.js
@@ -1,17 +1,4 @@
 import './setup.js';
 import app from './app.js';
 
-let envPort;
-const nodeEnv = process.env.NODE_ENV;
-const isRunningLocally = nodeEnv === 'test' || nodeEnv === 'prod';
-const isRunningOnCloud = nodeEnv === 'cloud';
-
-if (isRunningLocally) {
-  envPort = 4000;
-}
-
-if (isRunningOnCloud) {
-  envPort = process.env.PORT;
-}
-
-app.listen(envPort);
+app.listen(process.env.PORT);
